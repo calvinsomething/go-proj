ce24b5e893187a1fa01595d14786646adafb17ff
Calvin Storoschuk <calvinsomething@gmail.com>
2022-07-31 14:24:09 -0400

    go tests setup

--- GO/server.go
+++ GO/server.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	// "github.com/joho/godotenv"
 )
 
 var (
@@ -13,54 +12,63 @@ var (
 )
 
 type (
-	handler func(http.ResponseWriter, *http.Request)
+	handler_t func(http.ResponseWriter, *http.Request)
 
-	multiplexer struct {
-		mux *http.ServeMux
-		middleware []handler
+	mux_t struct {
+		mux        *http.ServeMux
+		middleware []handler_t
 	}
 )
 
 func init() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 
-	// if err := godotenv.Load("../.env"); err != nil {
-	// 	log.Fatal(err)
-	// }
-
 	_port = os.Getenv("GO_PORT")
 }
 
 func main() {
-	middleware := []handler{
+	log.Printf("Listening on port %s...\n", _port)
+	log.Fatal(http.ListenAndServe(":"+_port, newMux()))
+}
+
+// Custom Mux
+func newMux() *mux_t {
+	middleware := []handler_t{
 		logger,
 	}
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", home)
+	// Routes
+	mux.HandleFunc("/err", errHandler)
+	mux.HandleFunc("/data", dataHandler)
 
-	log.Printf("Listening on port %s...\n", _port)
-	log.Fatal(http.ListenAndServe(":" + _port, &multiplexer{mux, middleware}))
+	return &mux_t{mux, middleware}
 }
 
-func (m *multiplexer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (m *mux_t) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	for _, mw := range m.middleware {
 		mw(w, r)
 	}
 	m.mux.ServeHTTP(w, r)
 }
 
+// Middleware
 func logger(w http.ResponseWriter, r *http.Request) {
 	log.Println("LOGGING:", r.URL.Path)
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
+// temp...
+func errHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(400)
 	encoder := json.NewEncoder(w)
-	err := encoder.Encode(struct{
+	err := encoder.Encode(struct {
 		Err string
 	}{"test err"})
 	if err != nil {
 		log.Println(err)
 	}
+}
+
+func dataHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hi"))
 }
--- GO/server_test.go
+++ GO/server_test.go
@@ -1,9 +1,53 @@
 package main
 
 import (
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func TestWrite(t *testing.T) {
-	
+const (
+	root = "localhost:8080"
+)
+
+var tests = []struct{
+	method string
+	route string
+	body io.Reader
+	handler handler_t
+	want string
+}{
+	{"GET", "/data", nil, dataHandler, "hi"},
 }
+
+func TestRoutes(t *testing.T) {
+	ts := httptest.NewServer(newMux())
+	defer ts.Close()
+
+	for _, tc := range tests {
+		t.Run(tc.route, func (t *testing.T) {
+			req, err := http.NewRequest(tc.method, ts.URL+tc.route, tc.body)
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+
+			client := &http.Client{}
+			res, err := client.Do(req)
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+
+			body, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+
+			if string(body) != tc.want {
+				t.Fatalf("got %q; want %q", body, tc.want)
+			}
+		})
+	}
+}
+
